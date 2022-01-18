adc7a18d5818f32fb2d2b09a74fba085d3c37c81
Vineet Kumar <vineet.kumar_cs20@gla.ac.in>
2022-01-18 10:38:21 +0530

    new adddition

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-num=10
-#print(num)
-Num1=90.8
+num=10  #to store integer
+print(num)
+Num1=90.8   #to store float
 print(Num1)
-Name="VineetKumar"
+Name="VineetKumar"  #to store name
 print(Name)
-type(Name)
+type(Name)  #type of data
 print(type)
-num3=num+Num1
+num3=num+Num1   #sum of float + interger
 print(num3 )
